9cfed087811d798b085ad2bd5be35895149e84f4
delna121 <94628992+delna121@users.noreply.github.com>
2022-03-02 01:11:24 -0800

    Add files via upload

---
+++ lambda fun.py
@@ -0,0 +1,6 @@
+area1 = lambda x: x ** 2
+print("Area of square of side (5) =", area1(5))
+area2 = lambda x, y: x * y
+print("Area of a rectangle (5,2) =", area2(5,2))
+area3 = lambda  b, h: 1/2*b*h
+print("Area of a triangle (2,6) =", area3(2,6))
