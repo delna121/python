e1a2df573521676ded4d301ce8d5de6586800fe9
delna121 <94628992+delna121@users.noreply.github.com>
2022-03-02 01:12:07 -0800

    Add files via upload

---
+++ no.of charachter.py
@@ -0,0 +1,9 @@
+string = input("Enter string ")
+count = 0;
+
+for i in range(0, len(string)):
+    if (string[i] != ' '):
+        count = count + 1;
+
+print("Total number of characters in a string: " + str(count));
+
